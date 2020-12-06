617f4f5565673c051086ed0706ab6050793092b3
Philipie <118045821+Philipie@users.noreply.github.com>
2020-12-06 12:07:52 +0000

    Modify gitignore

--- CoinpaprikaAPI/Client.cs
+++ CoinpaprikaAPI/Client.cs
@@ -6,4 +6,8 @@ using CoinpaprikaAPI.Utils;
 using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
-using System
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
