9e99bd390d15066f2cca5dc5d4746e137f0cbc6f
Philipie <118045821+Philipie@users.noreply.github.com>
2020-12-19 18:59:38 +0000

    see ChangeLog file

--- CoinpaprikaAPI/Client.cs
+++ CoinpaprikaAPI/Client.cs
@@ -43,4 +43,6 @@ namespace CoinpaprikaAPI
                 RequestUri = new Uri(requestUrl)
             };
 
-    
+            var response = await client.SendAsync(request).ConfigureAwait(false);
+
+            return 
