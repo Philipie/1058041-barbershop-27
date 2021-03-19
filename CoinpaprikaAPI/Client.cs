ed05dc9ee6bdc0f997bba92685cb53b7baf1c153
Philipie <118045821+Philipie@users.noreply.github.com>
2021-03-19 20:05:23 +0000

    Update rabbitmq-components.mk

--- CoinpaprikaAPI/Client.cs
+++ CoinpaprikaAPI/Client.cs
@@ -163,4 +163,7 @@ namespace CoinpaprikaAPI
             var request = new HttpRequestMessage()
             {
                 Method = HttpMethod.Get,
-                RequestUri = 
+                RequestUri = new Uri(requestUrl)
+            };
+
+            var response = await client.SendAsync(request).Confi
