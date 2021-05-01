164e7beb56525af4d66ac8ef738483a2fc05c1d9
Philipie <118045821+Philipie@users.noreply.github.com>
2021-05-01 23:03:28 +0000

    Addressed review comments.

--- CoinpaprikaAPI/Client.cs
+++ CoinpaprikaAPI/Client.cs
@@ -232,4 +232,7 @@ namespace CoinpaprikaAPI
         /// <param name="endTime">end point for historical data</param>
         /// <param name="limit">limit of result rows (max 5000)</param>
         /// <param name="quote">returned data quote (available values: usd, btc)</param>
-        public async Task<CoinPaprikaEntity<List<OhlcValue>>> GetHistoricalOhlcForCoinAsync(string id, DateTimeOffset startTime, DateTimeOffset endTime = default, int limit = 1000, string quote =
+        public async Task<CoinPaprikaEntity<List<OhlcValue>>> GetHistoricalOhlcForCoinAsync(string id, DateTimeOffset startTime, DateTimeOffset endTime = default, int limit = 1000, string quote = "USD")
+        {
+            if (string.IsNullOrWhiteSpace(id))
+                throw new Not
