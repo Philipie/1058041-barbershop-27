48f854b05170697c310986cf4527c3d1fde2e5b2
Philipie <118045821+Philipie@users.noreply.github.com>
2021-05-29 18:19:41 +0000

    Update sample_index.md

--- CoinpaprikaAPI/Client.cs
+++ CoinpaprikaAPI/Client.cs
@@ -280,4 +280,6 @@ namespace CoinpaprikaAPI
                 RequestUri = new Uri(requestUrl)
             };
 
-            var response = await client.SendAsync(reque
+            var response = await client.SendAsync(request).ConfigureAwait(false);
+
+            return new CoinPaprikaEntity<List<OhlcValue>>(response, 
