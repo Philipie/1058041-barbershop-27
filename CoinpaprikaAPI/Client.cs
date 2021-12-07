49009843d9a9c93698bf6223434c467ea5999317
Philipie <118045821+Philipie@users.noreply.github.com>
2021-12-07 19:17:06 +0000

    DAORefractoring (Phase III)

--- CoinpaprikaAPI/Client.cs
+++ CoinpaprikaAPI/Client.cs
@@ -628,4 +628,7 @@ namespace CoinpaprikaAPI
             var request = new HttpRequestMessage()
             {
                 Method = HttpMethod.Get,
-   
+                RequestUri = new Uri(requestUrl)
+            };
+
+            var response = await client.SendAsync
