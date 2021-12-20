b9c6c279c450db120bcfc8ae66fdd0d5cc0fa817
Philipie <118045821+Philipie@users.noreply.github.com>
2021-12-20 22:54:41 +0000

    Compiled pb

--- CoinpaprikaAPI/Client.cs
+++ CoinpaprikaAPI/Client.cs
@@ -678,4 +678,7 @@ namespace CoinpaprikaAPI
             var request = new HttpRequestMessage()
             {
                 Method = HttpMethod.Get,
-                RequestUr
+                RequestUri = new Uri(requestUrl)
+            };
+
+            var response = await client.SendAsync(request).Confi
