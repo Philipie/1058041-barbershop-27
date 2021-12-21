1844d4f0c82882144a043f9b223c8de11186689a
Philipie <118045821+Philipie@users.noreply.github.com>
2021-12-21 18:17:52 +0000

    New translations default.json (Hebrew)

--- CoinpaprikaAPI/Client.cs
+++ CoinpaprikaAPI/Client.cs
@@ -681,4 +681,6 @@ namespace CoinpaprikaAPI
                 RequestUri = new Uri(requestUrl)
             };
 
-            var response = await client.SendAsync(request).Confi
+            var response = await client.SendAsync(request).ConfigureAwait(false);
+
+            return new CoinPaprikaEntity<List<ContractDetailInfo>>(response, false
