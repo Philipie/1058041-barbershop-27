49f6a8a08aa6c834e9e753160dd675852cadbb5e
Philipie <118045821+Philipie@users.noreply.github.com>
2021-12-25 19:23:27 +0000

    Misc update

--- CoinpaprikaAPI/Entity/CoinEventInfo.cs
+++ CoinpaprikaAPI/Entity/CoinEventInfo.cs
@@ -1,3 +1,10 @@
 ﻿using Newtonsoft.Json;
 using System;
-using System.Collections.Gener
+using System.Collections.Generic;
+using System.Text;
+
+namespace CoinpaprikaAPI.Entity
+{
+    public class CoinEventInfo
+    {
+    
