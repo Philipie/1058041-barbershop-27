276f4706b0cea33a624722c396de4b4d59fbb4d9
Philipie <118045821+Philipie@users.noreply.github.com>
2022-03-17 18:21:58 +0000

    - localization

--- CoinpaprikaAPI/Entity/IcoInfo.cs
+++ CoinpaprikaAPI/Entity/IcoInfo.cs
@@ -7,4 +7,8 @@ namespace CoinpaprikaAPI.Entity
 {
     public class IcoInfo
     {
-        [
+        [JsonProperty("id")]
+        public string Id { get; set; }
+
+        [JsonProperty("name")]
+        public string N
