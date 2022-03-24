f4245917dbcc3935a79356e1d2033912e6561a71
Philipie <118045821+Philipie@users.noreply.github.com>
2022-03-24 20:40:12 +0000

    update Tbody

--- CoinpaprikaAPI/Entity/MarketInfo.cs
+++ CoinpaprikaAPI/Entity/MarketInfo.cs
@@ -19,3 +19,35 @@ namespace CoinpaprikaAPI.Entity
 
         [JsonProperty("base_currency_id")]
         public string BaseCurrencyId { get; set; }
+
+        [JsonProperty("base_currency_name")]
+        public string BaseCurrencyName { get; set; }
+
+        [JsonProperty("quote_currency_id")]
+        public string QuoteCurrencyId { get; set; }
+
+        [JsonProperty("quote_currency_name")]
+        public string QuoteCurrencyName { get; set; }
+
+        [JsonProperty("market_url")]
+        public Uri MarketUrl { get; set; }
+
+        [JsonProperty("category")]
+        public string Category { get; set; }
+
+        [JsonProperty("fee_type")]
+        public string FeeType { get; set; }
+
+        [JsonProperty("outlier")]
+        public bool Outlier { get; set; }
+
+        [JsonProperty("adjusted_volume_24h_share")]
+        public double AdjustedVolume24HShare { get; set; }
+
+        [JsonProperty("quotes")]
+        public Dictionary<string, MarketQuoteInfo> Quotes { get; set; }
+
+        [JsonProperty("last_updated")]
+        public DateTimeOffset LastUpdated { get; set; }
+    }
+}
---
+++ CoinpaprikaAPI/Entity/ParentInfo.cs
@@ -0,0 +1,20 @@
+
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CoinpaprikaAPI.Entity
+{
+    public class ParentInfo
+    {
+        [JsonProperty("id")]
+        public string Id { get; set; }
+
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        [JsonProperty("symbol")]
+        public string Symbol { get; set; }
+    }
+}
