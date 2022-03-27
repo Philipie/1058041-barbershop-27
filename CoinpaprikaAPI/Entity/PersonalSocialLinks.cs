20542e7305d8f4a1350ddc647ca5f1b7b7cd5305
Philipie <118045821+Philipie@users.noreply.github.com>
2022-03-27 18:48:58 +0000

    new events

--- CoinpaprikaAPI/Entity/PersonalSocialLinks.cs
+++ CoinpaprikaAPI/Entity/PersonalSocialLinks.cs
@@ -6,4 +6,6 @@ using System.Text;
 namespace CoinpaprikaAPI.Entity
 {
     public class PersonalSocialLinks
-  
+    {
+        [JsonProperty("github")]
+        public List<PersonalSocialLinkInfo> Github { get; s
