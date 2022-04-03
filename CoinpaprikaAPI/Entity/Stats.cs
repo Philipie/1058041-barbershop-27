4c9c4c5a3ba6b594cefd5cb0c8d4ad000e7d5f88
Philipie <118045821+Philipie@users.noreply.github.com>
2022-04-03 06:15:10 +0000

    net: dst: provide accessor function to dst->xfrm

--- CoinpaprikaAPI/Entity/Stats.cs
+++ CoinpaprikaAPI/Entity/Stats.cs
@@ -8,4 +8,7 @@ namespace CoinpaprikaAPI.Entity
         [JsonProperty("members", NullValueHandling = NullValueHandling.Ignore)]
         public long? Members { get; set; }
 
-        [JsonProperty("followers", NullV
+        [JsonProperty("followers", NullValueHandling = NullValueHandling.Ignore)]
+        public long? Followers { get; set; }
+    }
+}
