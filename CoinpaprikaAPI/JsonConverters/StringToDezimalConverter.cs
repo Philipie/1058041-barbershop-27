68650be79ed0479540c02906dc4d1effeadfebac
Philipie <118045821+Philipie@users.noreply.github.com>
2022-05-04 15:16:23 +0000

    wot

--- CoinpaprikaAPI/JsonConverters/StringToDezimalConverter.cs
+++ CoinpaprikaAPI/JsonConverters/StringToDezimalConverter.cs
@@ -5,4 +5,6 @@ namespace CoinpaprikaAPI.JsonConverters
 {
     public class StringToDezimalConverter : JsonConverter
     {
-        public override bool CanConvert(Type t) => t == typeof(decimal) || t == typeof(decimal
+        public override bool CanConvert(Type t) => t == typeof(decimal) || t == typeof(decimal?);
+
+        public override object ReadJson(JsonReader reader, Type t, object existi
