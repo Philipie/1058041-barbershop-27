1435e635480f3da9d5145b4c5cadb92eaf467409
Philipie <118045821+Philipie@users.noreply.github.com>
2022-05-18 21:17:50 +0000

    Updates doc

--- CoinpaprikaAPI/JsonConverters/StringToIntConverter.cs
+++ CoinpaprikaAPI/JsonConverters/StringToIntConverter.cs
@@ -9,4 +9,7 @@ namespace CoinpaprikaAPI.JsonConverters
     {
         public override bool CanConvert(Type t) => t == typeof(int) || t == typeof(int?);
 
-        public override object ReadJson(JsonReader reader, Type t, object existingValue, JsonSerializer serial
+        public override object ReadJson(JsonReader reader, Type t, object existingValue, JsonSerializer serializer)
+        {
+            if (reader.TokenType == JsonToken.Null) return null;
+            var
