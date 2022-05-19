239457ff684b64dd7d2ec39754768b4c19af564a
Philipie <118045821+Philipie@users.noreply.github.com>
2022-05-19 19:50:51 +0000

    Forward port zendframework/zf2#3681

--- CoinpaprikaAPI/JsonConverters/StringToIntConverter.cs
+++ CoinpaprikaAPI/JsonConverters/StringToIntConverter.cs
@@ -12,4 +12,5 @@ namespace CoinpaprikaAPI.JsonConverters
         public override object ReadJson(JsonReader reader, Type t, object existingValue, JsonSerializer serializer)
         {
             if (reader.TokenType == JsonToken.Null) return null;
-            var
+            var value = serializer.Deserialize<string>(reader);
+            if (string.IsNullOrWhiteSpace(va
