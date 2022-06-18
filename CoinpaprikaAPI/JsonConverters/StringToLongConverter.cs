82a9819249b98110082d53fcaef25d7357034d38
Philipie <118045821+Philipie@users.noreply.github.com>
2022-06-18 21:27:41 +0000

    Update run_analysis.R

--- CoinpaprikaAPI/JsonConverters/StringToLongConverter.cs
+++ CoinpaprikaAPI/JsonConverters/StringToLongConverter.cs
@@ -8,3 +8,6 @@ namespace CoinpaprikaAPI.JsonConverters
         public override bool CanConvert(Type t) => t == typeof(long) || t == typeof(long?);
 
         public override object ReadJson(JsonReader reader, Type t, object existingValue, JsonSerializer serializer)
+        {
+            if (reader.TokenType == JsonToken.Null) return null;
+        
