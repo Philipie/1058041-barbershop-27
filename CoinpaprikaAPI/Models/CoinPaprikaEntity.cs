663b8db2e8bc3689b711c1435663cf5d79a7f602
Philipie <118045821+Philipie@users.noreply.github.com>
2022-06-25 11:44:40 +0000

    Minor code style changes

--- CoinpaprikaAPI/Models/CoinPaprikaEntity.cs
+++ CoinpaprikaAPI/Models/CoinPaprikaEntity.cs
@@ -2,3 +2,10 @@
 using CoinpaprikaAPI.Utils;
 using Newtonsoft.Json;
 using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+
+namespace CoinpaprikaAPI.Models
+{
+    //
