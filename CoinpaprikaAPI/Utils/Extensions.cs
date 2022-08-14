83ee785d65c35f942056789ace1adb0f0f1553f0
Philipie <118045821+Philipie@users.noreply.github.com>
2022-08-14 14:45:59 +0000

    drm/ttm: Choose a pool to shrink correctly in ttm_dma_pool_shrink_scan().

--- CoinpaprikaAPI/Utils/Extensions.cs
+++ CoinpaprikaAPI/Utils/Extensions.cs
@@ -45,3 +45,70 @@ namespace CoinpaprikaAPI.Utils
                 case TickerInterval.FourtyFiveMinutes:
                     return "45m";
                 case TickerInterval.OneHour:
+                    return "1h";
+                case TickerInterval.TwoHours:
+                    return "2h";
+                case TickerInterval.ThreeHours:
+                    return "3h";
+                case TickerInterval.SixHours:
+                    return "6h";
+                case TickerInterval.TwelveHours:
+                    return "12h";
+                case TickerInterval.TwentyFourHours:
+                    return "24h";
+                case TickerInterval.OneDay:
+                    return "1d";
+                case TickerInterval.SevenDays:
+                    return "7d";
+                case TickerInterval.FourteenDays:
+                    return "14d";
+                case TickerInterval.ThirtyDays:
+                    return "30d";
+                case TickerInterval.NinetyDays:
+                    return "90d";
+                case TickerInterval.ThreeHundredSixtyFiveDays:
+                    return "365d";
+            }
+
+            return null;
+        }
+
+        public static bool IsSupportedQuoteSymbol(this string quoteSymbol)
+        {
+            if (!string.IsNullOrWhiteSpace(quoteSymbol))
+            {
+                var allowed = new[] { "BTC", "ETH", "USD", "EUR", "PLN", "KRW", "GBP", "CAD", "JPY", "RUB", "TRY", "NZD", "AUD", "CHF", "UAH", "HKD", "SGD", "NGN", "PHP", "MXN", "BRL", "THB", "CLP", "CNY", "CZK", "DKK", "HUF", "IDR", "ILS", "INR", "MYR", "NOK", "PKR", "SEK", "TWD", "ZAR", "VND", "BOB", "COP", "PEN", "ARS", "ISK" };
+
+                return allowed.Contains(quoteSymbol);
+            }
+            else
+                return false;
+        }
+
+        public static bool IsSupportedTagField(this string tagField)
+        {
+            if (!string.IsNullOrWhiteSpace(tagField))
+            {
+                var allowed = new[] { "coins", "icos" };
+
+                return allowed.Contains(tagField);
+            }
+            else
+                return false;
+        }
+
+        public static string AddParameterToUrl(this string url, string parameterName, object parameterValue)
+        {
+            if (url.Contains("?"))
+            {
+                return $"{url}&{parameterName}={parameterValue.ToString()}";
+            }
+            else
+            {
+                return $"{url}?{parameterName}={parameterValue.ToString()}";
+            }
+        }
+
+
+    }
+}
