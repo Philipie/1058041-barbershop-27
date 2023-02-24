84475c5338db9ca95cd5d4130fc43cb2be31e64f
Philipie <118045821+Philipie@users.noreply.github.com>
2023-02-24 19:49:47 +0000

    edited index.html

--- Tester/Properties/AssemblyInfo.cs
+++ Tester/Properties/AssemblyInfo.cs
@@ -4,4 +4,7 @@ using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
-// associated with an assembl
+// associated with an assembly.
+[assembly: AssemblyTitle("Tester")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration
