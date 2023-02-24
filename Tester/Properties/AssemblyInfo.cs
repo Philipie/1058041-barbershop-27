1e19287ecfd5d0b70cf5cb575433ddcaa3aac08a
Philipie <118045821+Philipie@users.noreply.github.com>
2023-02-24 20:57:45 +0000

    use travis

--- Tester/Properties/AssemblyInfo.cs
+++ Tester/Properties/AssemblyInfo.cs
@@ -7,4 +7,7 @@ using System.Runtime.InteropServices;
 // associated with an assembly.
 [assembly: AssemblyTitle("Tester")]
 [assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tester")]
+[assembly: Assembly
